f3a0d7f05863189aa8e668c9f66c375f8b9504f6
Albin Rajesh <albinedits1@gmail.com>
2026-09-13 22:01:08 +0530

    Remove image scroll section

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import Footer from './components/Footer'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import Projects from './components/Projects'
-import ScrollShowcase from './components/ScrollShowcase'
 import Skills from './components/Skills'
 
 function App() {
@@ -57,7 +56,6 @@ function App() {
         <About />
         <Skills />
         <Projects />
-        <ScrollShowcase />
         <Experience />
         <Achievements />
         <Contact />
